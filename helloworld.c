b7a6cb3a1179e0654dfda81e5ba5f9407d113ee6
Jie <jietan25@gmail.com>
2014-02-13 12:10:35 -0700

    Jie--Check out and commit for Q5

--- helloworld.c
+++ helloworld.c
@@ -5,10 +5,11 @@
 main()
 {
 	int Lcounter = 0;
-	for (Lcounter = 0; Lcounter < 3; Lcounter++)
+	for (Lcounter = 0; Lcounter < 5; Lcounter++)
 	{
 		printf("Hello World");
 		printf("\nHello World from Zelpha");
+		printf("\nHello World from Jie");
 	}
 	return 0;
 }
